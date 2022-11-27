668704da820ef476fc089f7c1cb0805037454e13
George Tsagiannis <gtsagiannis@gmail.com>
2022-11-27 03:47:55 +0000

    Add review to restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [ :index, :show ] do
+    resources :reviews, only: :create
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
